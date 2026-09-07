ef743ce8791ac4a02ec4f4fb1d46f7bb40953cd9
Piyush Saxena <piyushsaxena168@gmail.com>
2026-09-07 17:32:42 +0530

    Update ResearchController.java

--- SR/SR/src/main/java/com/Nexquira/SR/ResearchController.java
+++ SR/SR/src/main/java/com/Nexquira/SR/ResearchController.java
@@ -1,19 +1,36 @@
 package com.Nexquira.SR;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/research")
 @CrossOrigin(origins = "*")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ResearchController {
+
     private final ResearchService researchService;
+    private final ResearchNoteRepository noteRepository;
+    private final UserRepository userRepository;
 
     @PostMapping("/process")
-    public ResponseEntity<String>processContent(@RequestBody ResearchRequest request){
-       String result = researchService.processContent(request);
+    public ResponseEntity<String> processContent(@RequestBody ResearchRequest request,
+                                                   Authentication authentication) {
+        String result = researchService.processContent(request);
+
+        String email = authentication.getName();
+        User user = userRepository.findByEmail(email).orElseThrow();
+
+        ResearchNote note = new ResearchNote();
+        note.setSourceContent(request.getContent());
+        note.setAiSummary(result);
+        note.setOperation(request.getOperation());
+        note.setUser(user);
+
+        noteRepository.save(note);
+
         return ResponseEntity.ok(result);
     }
 }
